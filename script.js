be93d6344320fb45179f2cdd33e87119670e8c66
adamdotg <adamdotg@gmail.com>
2019-12-04 08:18:26 -0600

    green active working but weird. probably not right.

--- script.js
+++ script.js
@@ -3,28 +3,41 @@ window.addEventListener("load", function(){
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
         response.json().then(function(json){
             const container = document.getElementById("container");
+            //sort by hoursInSpace: declare empty array > 
             for (let i = 0; i<json.length; i++){
-                container.innerHTML += `
-                <div class="astronaut">
-                    <div class="bio">
-                        <h3>${json[i].firstName} ${json[i].lastName}</h3>
-                        <ul>
-                            <li>Hours in space: ${json[i].hoursInSpace}</li>
-                            <li id="color" style="color: black;">Active: ${json[i].active}</li>
-                            <li>Skills: ${json[i].skills}</li>
-                        </ul>
-                    </div>
-                    <img class="avatar" src="${json[i].picture}">
-                </div>
-                `;
                 if (json[i].active){
-                    document.getElementById("color").style.color = "green";
-                    //let activeColor = document.querySelector(".color");
-                   // activeColor.style.color = "green";
-                    console.log("looking for color class", json[i].firstName);
-                    //console.log("?", activeColor.style.color);
+                    container.innerHTML += `
+                    <div class="astronaut">
+                        <div class="bio">
+                            <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                            <ul>
+                                <li>Hours in space: ${json[i].hoursInSpace}</li>
+                                <li id="color" style="color: green;">Active: ${json[i].active}</li>
+                                <li>Skills: ${json[i].skills}</li>
+                            </ul>
+                        </div>
+                        <img class="avatar" src="${json[i].picture}">
+                    </div>
+                    `;
+                } else {
+                    container.innerHTML += `
+                    <div class="astronaut">
+                        <div class="bio">
+                            <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                            <ul>
+                                <li>Hours in space: ${json[i].hoursInSpace}</li>
+                                <li id="color" style="color: black;">Active: ${json[i].active}</li>
+                                <li>Skills: ${json[i].skills}</li>
+                            </ul>
+                        </div>
+                        <img class="avatar" src="${json[i].picture}">
+                    </div>
+                    `;
                 }
-                //if active then active:true = green or something
+                //document.getElementById("color").style.color = "green";
+                //let activeColor = document.querySelector(".color");
+                // activeColor.style.color = "green";
+                //console.log("looking for color class", json[i].firstName);
             }
         });
       //container.innerHTML = 
